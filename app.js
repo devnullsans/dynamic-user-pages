c6f97d6e7530954dfe5547f2aa3a3419ba48df34
PaulS <dedsecdarknet@gmail.com>
2021-10-16 20:56:56 +0530

    static routes done

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const app = express();
 
 const {
 	PORT = 5000,
-	TTL = 1e4,
+	TTL = 7.2e6,
+	SSN = 'sid',
 	KEY = 'a4f087f1-c837-4740-8ed2'
 } = process.env;
 
@@ -17,17 +18,19 @@ const users = [
 
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
-
+/*
 // app.use((req, res, next) => {
 // 	res.locals.userValue = null;
 // 	next();
 // });
+*/
 
 app.use(session({
 	resave: false,
 	saveUninitialized: false,
 	cookie: { maxAge: TTL },
 	secret: KEY,
+	name: SSN
 }));
 
 app.set('view engine', 'ejs');
@@ -39,35 +42,53 @@ app.get('/', (req, res) => {
 	res.render('index', { userName: user?.name });
 	console.log(user?.id, 'Accessed Index page');
 });
+
 // /login route res render login.ejs
-app.get('/login', redirectHome, (req, res) => {
-	res.render('login');
-});
+app.get('/login', redirectHome, (req, res) => res.render('login'));
 // /register route res render register.ejs
-app.get('/register', redirectHome, (req, res) => {
-	res.render('register');
-});
+app.get('/register', redirectHome, (req, res) => res.render('register'));
 // /home route res render home.ejs
-app.get('/home', redirectLogin, (req, res) => {
+
+app.get('/home', redirectLogin, (req, res) => { // Always extract user from session
 	const { user } = req.session;
-	res.render('home', { userName: user.name });
+	res.render('home', {
+		userName: user.name,
+		userPages: user.pages
+	});
 	console.log(user.id, 'Accessed Home page');
 });
-// /route roiute res render route.ejs
-app.get('/route', redirectLogin, (req, res) => {
-	req.render('route');
+
+// /route route res render route.ejs
+app.get('/route', redirectLogin, (req, res) => res.render('route', { pageID: null }));
+// /route route res render route.ejs with data | for editing specific route data
+app.get('/route/:id', redirectLogin, (req, res) => {
+	const { id } = (req.params);
+	const { user } = (req.session);
+	const page = user.pages.find(pg => Number(id) === pg.id);
+	if (!page) return res.status(403).end();
+	// TODO render route form with data and save/cancel button
 });
 
 // /logout redirect to /
 app.post('/logout', redirectLogin, (req, res) => {
+	req.session.destroy(err => {
+		if (err) {
+			console.log(err);
+			return res.redirect('/home');
+		}
+		res.clearCookie(SSN);
+		res.redirect('/');
+	});
 
 });
 // /login redirect to /home OR json res error
 app.post('/login', redirectHome, (req, res) => {
 	console.log(req.body); // { email: 'tom@gmail.com', password: 'secret' }
 	const { email, password } = req.body;
+	// IFDEF params validation
 	const user = users.find(usr => usr.email === email && usr.password === password);
 	if (!user) return res.status(401).end();
+	// TODO device verification via email
 	req.session.user = user;
 	res.redirect('/home');
 });
@@ -75,22 +96,34 @@ app.post('/login', redirectHome, (req, res) => {
 app.post('/register', redirectHome, (req, res) => {
 	console.log(req.body); // { name: 'Tom', email: 'tom@gmail.com', password: 'secret' }
 	const { name, email, password } = req.body;
+	// TODO params validation
 	const avail = users.find(usr => usr.email === email);
-	if (avail) return res.status(409).end();
+	if (avail)
+		return res.status(409).end();
+	// TODO email verification
 	const user = {
 		name, email,
 		password, pages: [],
-		id: users.length
+		id: users.length + 1
 	};
 	users.push(user);
 	req.session.user = user;
 	res.redirect('/home');
 });
 app.post('/route', redirectLogin, (req, res) => {
-	console.log(req.body);
+	console.log(req.body);// { business: 'Hello', service: 'World!' }
+	const { business, service } = req.body;
+	//TODO param validation
+	const { user } = req.session;
+	const page = {
+		business, service,
+		id: user.pages.length + 1
+	};
+	user.pages.push(page);
+	res.redirect('/home');
 });
 
-app.listen(5000, () => console.log('running on port 5000'));
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
 
 function redirectLogin(req, res, next) {
 	if (!req.session.user) res.redirect('/login');
